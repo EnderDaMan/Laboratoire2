a1403129a6d53a75679ec87339eacf92915f20cf
Tristan Berthiaume <tristan.007@live.ca>
2023-10-07 21:09:13 -0400

    fe

---
+++ controllers/MathsController.js
@@ -0,0 +1,9 @@
+import MathsModel from '../models/maths.js';
+import Repository from '../models/repository.js';
+import Controller from './Controller.js';
+
+export default class MathsController extends Controller {
+    constructor(HttpContext) {
+        super(HttpContext, new Repository(new MathsModel()));
+    }
+}
